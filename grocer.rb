f4d604b63085b43976d872b68d95f3deb4116ba2
Matt Werner <mrwerner392@gmail.com>
2019-07-17 23:50:58 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -49,7 +49,7 @@ end
 def checkout(cart, coupons)
   #consolidated_cart = consolidate_cart(cart)
   #consolidated_cart_w_coupons = apply_coupons(consolidated_cart, coupons)
-  consolidated_cart_w_coupons_and_discounts = apply_clearance( apply_coupons( consolidate_cart(cart), coupons ) ) 
+  consolidated_cart_w_coupons_and_discounts = apply_clearance( apply_coupons( consolidate_cart(cart), coupons ) )
 
 
 end
