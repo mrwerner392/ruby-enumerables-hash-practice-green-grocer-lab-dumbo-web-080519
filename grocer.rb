a06f487c1be2995aae2f2ce3f739454ae0ef8b85
Matt Werner <mrwerner392@gmail.com>
2019-07-17 23:56:28 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -50,7 +50,7 @@ def checkout(cart, coupons)
   #consolidated_cart = consolidate_cart(cart)
   #consolidated_cart_w_coupons = apply_coupons(consolidated_cart, coupons)
   final_cart = apply_clearance( apply_coupons( consolidate_cart(cart), coupons ) )
-  total = final_cart.reduce(0) do memo, (key, value)|
+  total = final_cart.reduce(0) do |memo (key, value)|
     memo += (value[:price] * value[:count])
     memo
   end
